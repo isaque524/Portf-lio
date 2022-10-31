693744e299774cafe17170e19c3461c89449b022
isaque524 <isaquesilvadamasceno@gmail.com>
2022-10-31 10:58:33 -0300

    seção sobre e habilidades

--- static/script.js
+++ static/script.js
@@ -9,3 +9,17 @@ menuMobile.addEventListener('click', () => {
         : menuMobile.classList.replace("bi-x", "bi-list");
     body.classList.toggle("menu-nav-active")
 })
+
+/*Fechar o menu ao click em algum item e muda o icone*/
+
+const navItem = document.querySelectorAll('.nav-item')
+
+navItem.forEach(item => {
+    item.addEventListener("click", () => {
+        if (body.classList.contains("menu-nav-active")) {
+            body.classList.remove("menu-nav-active")
+            menuMobile.classList.replace("bi-x", "bi-list");
+        }
+    })
+
+})
