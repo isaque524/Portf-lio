d4254c8a35e55b01072a4f1ee17cd8e479a412a3
isaque524 <isaquesilvadamasceno@gmail.com>
2022-11-03 16:13:00 -0300

    config

--- app.py
+++ app.py
@@ -45,7 +45,8 @@ def send():
         msg = Message(
             subject=f'{formContato.nome} te enviou uma mensagem no portfólio',
             sender=app.config.get("MAIL_USERNAME"),
-            recipients=[app.config.get("MAIL_USERNAME")],
+            recipients=['isaquesilvadamasceno@gmail.com',
+                        app.config.get("MAIL_USERNAME")],
             body=f'''
             
             {formContato.nome} com o e-mail {formContato.email}, te enviou a seguinte mensagem:
